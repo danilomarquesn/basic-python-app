db2c0e7c68e7b390e0c352f663835dfffe00056f
danilomarques.n <danilomarques.n@gmail.com>
2022-06-17 02:21:38 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 sudo apt-get update -y
+sudo apt-get install git ansible -y
+git clone https://github.com/danilomarquesn/basic-python-app.git /tmp/python-application/
+cd /tmp/python-application/
+ansible-playbook playbook.yaml
